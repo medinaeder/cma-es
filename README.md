f4996befeea49375728af19a8f26c735e7cc02c5
Eder Medina <e_medina@g.harvard.edu>
2020-05-07 22:45:17 -0400

    making this a module

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CMA-ES
 Implementation of the CMA-ES algorithm using mpi4py for parallelization
+
+# Need to add a setup file and an __init__ file
+
+# Then this should work as a standalone module
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+    
+setup(name = "cma-es",
+      version='0.1',
+      description='A plugin to say hello in several ways',
+      url='github?',
+      author='Your Name',
+      author_email='your.name@example.com',
+      license='MIT',
+      packages=['cma-es'],
+      zip_safe=False)
+           
