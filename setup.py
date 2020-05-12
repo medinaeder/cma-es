9b818a189c4b0d30785758b087a159d28ba06eb4
Eder Medina <e_medina@g.harvard.edu>
2020-05-12 09:57:18 -0400

    Adding the setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
     
-setup(name = "cma_es",
+setup(name = "bifcma",
       version='0.0.1',
-      description='Replicate of CMA-ES',
+      description='Replicate of CMA-ES to be used with bifurcation',
       url='github?',
       author='Eder Medina',
       author_email='e_medina@g.harvard.edu',
       license='MIT',
-      packages=['cma_es'],
+      packages=['bifcma'],
       zip_safe=False)
            
